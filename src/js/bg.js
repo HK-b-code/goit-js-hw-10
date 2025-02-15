486e67c5e5eb9d1015aabefc40e1dcb0f5d62571
HK-b-code <gkros13@gmail.com>
2025-02-15 16:55:14 +0100

    2

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,12 +1,12 @@
 const images = [
-  '../bg/1.webp',
-  '../bg/2.webp',
-  '../bg/3.webp',
-  '../bg/4.webp',
-  '../bg/5.webp',
-  '../bg/6.webp',
-  '../bg/7.webp',
-  '../bg/8.webp',
+  '/bg/1.webp',
+  '/bg/2.webp',
+  '/bg/3.webp',
+  '/bg/4.webp',
+  '/bg/5.webp',
+  '/bg/6.webp',
+  '/bg/7.webp',
+  '/bg/8.webp',
 ];
 
 let currentImageIndex = 0;
