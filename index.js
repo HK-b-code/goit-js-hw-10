e97ed53bf12bdefbeb373c44485ae6039d0bbc85
HK-b-code <gkros13@gmail.com>
2025-02-15 14:40:35 +0000

    Deploying to gh-pages from @ HK-b-code/goit-js-hw-10@d9fa2c1cae0b8d19af4a83b7c1d6b37e0ddd68f6 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/styles-D_F7Kjbb.js";const b=["../img/webp/1.webp","../img/webp/2.webp","../img/webp/3.webp","../img/webp/4.webp","../img/webp/5.webp","../img/webp/6.webp","../img/webp/7.webp","../img/webp/8.webp"];let e=0;function p(){document.body.style.backgroundImage=`url(${b[e]})`,e=(e+1)%b.length}setInterval(p,1e4);p();
+import"./assets/styles-D_F7Kjbb.js";const b=["./img/webp/1.webp","./img/webp/2.webp","./img/webp/3.webp","./img/webp/4.webp","./img/webp/5.webp","./img/webp/6.webp","./img/webp/7.webp","./img/webp/8.webp"];let e=0;function p(){document.body.style.backgroundImage=`url(${b[e]})`,e=(e+1)%b.length}setInterval(p,1e4);p();
 //# sourceMappingURL=index.js.map
